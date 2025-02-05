bbba097a12813ac3333093326e9b266e65cb075d
Gabriel <gabrielg-cruz@hotmail.com>
2025-02-05 15:52:19 -0300

    feat: add user entity atributes

--- src/main/java/com/QuickAuth/models/User.java
+++ src/main/java/com/QuickAuth/models/User.java
@@ -1,4 +1,49 @@
 package com.QuickAuth.models;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "user")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @NotBlank(message = "Email is mandatory")
+    @Column(nullable = false)
+    private String email;
+
+    @NotBlank(message = "Password is mandatory")
+    @Column(nullable = false)
+    private String password;
+
+    @NotBlank(message = "Username is mandatory")
+    @Column(nullable = false)
+    private String username;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
 }
