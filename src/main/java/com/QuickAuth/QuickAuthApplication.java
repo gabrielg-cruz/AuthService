eb1cab550e275bd23157657f258e5feb6f57ae4d
Gabriel <gabrielg-cruz@hotmail.com>
2025-02-05 13:50:13 -0300

    chore: initial commit

---
+++ src/main/java/com/QuickAuth/QuickAuthApplication.java
@@ -0,0 +1,13 @@
+package com.QuickAuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/QuickAuth/QuickAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.QuickAuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
