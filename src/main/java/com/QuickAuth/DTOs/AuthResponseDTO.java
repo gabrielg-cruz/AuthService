1fde6f33dbe79d1467cfcde029a9af171b914527
Gabriel <gabrielg-cruz@hotmail.com>
2025-02-06 13:33:59 -0300

    feat: add getter and setter methods

--- src/main/java/com/QuickAuth/DTOs/AuthResponseDTO.java
+++ src/main/java/com/QuickAuth/DTOs/AuthResponseDTO.java
@@ -3,13 +3,6 @@ package com.QuickAuth.DTOs;
 public class AuthResponseDTO {
     private String token;
 
-    public AuthResponseDTO() {
-    }
-
-    public AuthResponseDTO(String token) {
-        this.token = token;
-    }
-
     public String getToken() {
         return token;
     }
