384248f82134865f324467ed6fa18a8ad0e14887
Gabriel <gabrielg-cruz@hotmail.com>
2025-02-06 13:34:43 -0300

    feat: add getter and setter methods

--- src/main/java/com/QuickAuth/DTOs/LoginRequestDTO.java
+++ src/main/java/com/QuickAuth/DTOs/LoginRequestDTO.java
@@ -1,4 +1,30 @@
 package com.QuickAuth.DTOs;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public class LoginRequestDTO {
+
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Email is invalid")
+    private String email;
+
+    @NotBlank(message = "Password is mandatory")
+    private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
