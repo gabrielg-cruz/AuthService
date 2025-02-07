04a19d3ee3094582e058fe4d275a3e14f23cefe1
Gabriel <gabrielg-cruz@hotmail.com>
2025-02-07 14:18:22 -0300

    feat: add new methods to IUserServices

--- src/main/java/com/QuickAuth/interfaces/IUserServices.java
+++ src/main/java/com/QuickAuth/interfaces/IUserServices.java
@@ -6,5 +6,6 @@ import com.QuickAuth.models.User;
 public interface IUserServices {
     UserDTO createUser(UserDTO userDTO);
     UserDTO getUserById(int id);
+    void    deleteUser(int id);
     User    findUserEntityById(int id);
 }
