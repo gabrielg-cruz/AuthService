1a58dffadc4ee18c2120b7e8299ae84c84af55e4
Gabriel <gabrielg-cruz@hotmail.com>
2025-02-07 13:10:11 -0300

    feat: add User Repository

---
+++ src/main/java/com/QuickAuth/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.QuickAuth.repository;
+
+import com.QuickAuth.models.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+}
