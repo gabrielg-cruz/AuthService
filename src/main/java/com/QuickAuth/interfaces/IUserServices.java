d11eab03929100a780bd2fbd65c3edf742b1bcbd
Gabriel <gabrielg-cruz@hotmail.com>
2025-02-07 13:09:39 -0300

    feat: add Interface to User methods

---
+++ src/main/java/com/QuickAuth/interfaces/IUserServices.java
@@ -0,0 +1,10 @@
+package com.QuickAuth.interfaces;
+
+import com.QuickAuth.DTOs.UserDTO;
+import com.QuickAuth.models.User;
+
+public interface IUserServices {
+    UserDTO createUser(UserDTO userDTO);
+    UserDTO getUserById(int id);
+    User    findUserEntityById(int id);
+}
