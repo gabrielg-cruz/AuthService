d67b32d0306af9c57cb458b97f12dd433b5478a0
Gabriel <gabrielg-cruz@hotmail.com>
2025-02-09 15:53:46 -0300

    fix: correct missuse of spring notation

--- src/main/java/com/QuickAuth/mapper/UserMapper.java
+++ src/main/java/com/QuickAuth/mapper/UserMapper.java
@@ -4,9 +4,9 @@ import com.QuickAuth.DTOs.UserDTO;
 import com.QuickAuth.models.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
-import org.springframework.web.bind.annotation.Mapping;
 
-@Mapper(componentModel = "spring")
+
+@Mapper
 public interface UserMapper {
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
 
